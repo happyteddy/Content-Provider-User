1e9d6d8c98cad5ae89c0836ff7332b3c2f90f401
Angelina_Wu <Angelina_Wu@asus.com>
2016-05-31 11:10:49 +0800

    getSupportLoaderManager() -> getLoaderManager()

--- app/src/main/java/com/example/angelina_wu/bookstoreuser/UserActivity.java
+++ app/src/main/java/com/example/angelina_wu/bookstoreuser/UserActivity.java
@@ -1,14 +1,15 @@
 package com.example.angelina_wu.bookstoreuser;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
+import android.content.Loader;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
+
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
@@ -56,7 +57,7 @@ public class UserActivity extends AppCompatActivity  implements LoaderManager.Lo
         mUserName = pref.getString(MainActivity.USER_INFO_PREFS_NAME, null);
         mUserId = pref.getInt(MainActivity.USER_INFO_PREFS_ID, 0);
         Bundle b = new Bundle();
-        getSupportLoaderManager().initLoader(LOADER_1, b, this);
+        getLoaderManager().initLoader(LOADER_1, b, this);
         display();
     }
 
@@ -111,7 +112,7 @@ public class UserActivity extends AppCompatActivity  implements LoaderManager.Lo
             listView.setAdapter(mDataAdapter);
             Bundle b = new Bundle();
             b.putInt(STATE, DISPLAY_BUY);
-            getSupportLoaderManager().restartLoader(LOADER_1, b, this);
+            getLoaderManager().restartLoader(LOADER_1, b, this);
         }
     }
 
@@ -143,7 +144,7 @@ public class UserActivity extends AppCompatActivity  implements LoaderManager.Lo
             listView.setAdapter(mDataAdapter);
             Bundle b = new Bundle();
             b.putInt(STATE, DISPLAY_SELL);
-            getSupportLoaderManager().restartLoader(LOADER_1, b, this);
+            getLoaderManager().restartLoader(LOADER_1, b, this);
 
             listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
                 @Override
@@ -240,7 +241,7 @@ public class UserActivity extends AppCompatActivity  implements LoaderManager.Lo
             listView.setAdapter(mDataAdapter);
             Bundle b = new Bundle();
             b.putInt(STATE, DISPLAY);
-            getSupportLoaderManager().restartLoader(LOADER_1, b, this);
+            getLoaderManager().restartLoader(LOADER_1, b, this);
 
             listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
                 @Override
@@ -391,7 +392,7 @@ public class UserActivity extends AppCompatActivity  implements LoaderManager.Lo
             Bundle b = new Bundle();
             b.putString(BOOK_NAME,searchBook);
             b.putInt(STATE, SEARCH_BOOK);
-            getSupportLoaderManager().restartLoader(LOADER_1, b, this);
+            getLoaderManager().restartLoader(LOADER_1, b, this);
 
         }
 
@@ -444,7 +445,7 @@ public class UserActivity extends AppCompatActivity  implements LoaderManager.Lo
             b.putString(SELLER_NAME, searchSName);
             b.putString(SELLER_ID, searchSId);
             b.putInt(STATE, SEARCH_SELLER);
-            getSupportLoaderManager().restartLoader(LOADER_1, b, this);
+            getLoaderManager().restartLoader(LOADER_1, b, this);
         }
 
         TextView text = (TextView) findViewById(R.id.infoText);
